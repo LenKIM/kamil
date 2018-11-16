966fb0b32ab967fa890737db521479fd43642b85
len <joenggyu0@gmail.com>
2018-11-16 15:49:34 +0900

    fix: create const.py and move const vars

--- kamil/helpers/site_crawling.py
+++ kamil/helpers/site_crawling.py
@@ -10,6 +10,8 @@ from typing import List
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+from kamil.kamil_const import *
+
 
 def set_webdriver_options():
     options = webdriver.ChromeOptions()
@@ -30,17 +32,6 @@ class SiteCrawling:
 
     def start_crawling(self) -> List:
         set_webdriver_options()
-
-        input_args = "https://www.naver.com/"  # args 변경할 것
-
-        search_query = "효성계열사"
-        naver_site = 'https://www.naver.com/'
-        total_page_selector = '#main_pack > div.sp_company_list.sc._au_company_search._company_list > div.api_subject_bx > div.paging_area._page_navi > div > span.page_number > strong.total._total_page'
-        page_others_selector = '#main_pack > div.sp_company_list.sc._au_company_search._company_list > div.api_subject_bx > div.content_area > ul > ul > li > div > div.item_tit > a'
-        page_only_one_selector = '#main_pack > div.sp_company_list.sc._au_company_search._company_list > div.api_subject_bx > div.content_area > ul > li > div > div.item_tit > a'
-        arrow_element_xpath = '//*[@id="main_pack"]/div[2]/div[2]/div[4]/div/a[2]'
-        chrome_driver_path = '/Users/len/Downloads/chromedriver'
-        wait_time = 3
 
         driver = webdriver.Chrome(chrome_driver_path)
         driver.implicitly_wait(wait_time)
