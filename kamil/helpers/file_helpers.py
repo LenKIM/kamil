f4c4e0184ef71ad9572cd20630b98530afaa1b74
LenKIM <joenggyu0@gmail.com>
2018-11-23 14:38:43 +0900

    add helpers files

--- kamil/helpers/file_helpers.py
+++ kamil/helpers/file_helpers.py
@@ -1,7 +1,29 @@
 # -*- coding: utf-8 -*-
+import re
+
+from sklearn.feature_extraction.text import CountVectorizer
 
 
-class FileHelpers:
+def get_total_text_from_files():
+    with open('/Users/len/kamill/tests/textures/sentences.txt', 'r', encoding='utf8') as text_file:
+        list = []
+        origin_texts = []
+        for index, line in enumerate(text_file):
+            removed_special_char_line = re.sub('[^\w]', ' ', line)
+            list.append(removed_special_char_line)
+            origin_texts.append(line)
+        return list, origin_texts
 
-    def __init__(self) -> None:
-        super().__init__()
+
+def get_frequent_counted_companies_from_file():
+    removed_special_char, total_texts = get_total_text_from_files()
+    texts = []
+    for index, line in enumerate(removed_special_char):
+        words = [word for word in line.split() if '효성' in word]
+        new_str = ' '.join(words)
+        texts.append(new_str)
+
+    vect = CountVectorizer(ngram_range=(1, 1))
+    matrix = vect.fit_transform(texts)
+    freqs = [(word, matrix.getcol(idx).sum()) for word, idx in vect.vocabulary_.items()]
+    return freqs
--- kamil/helpers/stop_word_helpers.py
+++ kamil/helpers/stop_word_helpers.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
-class StopWordHelpers:
+from typing import List
 
-    def __init__(self) -> None:
-        super().__init__()
 
-    def load_stopword_from_file(self):
-        pass
+def get_stopwords_from_file() -> List:
+    """
+    불용어만 따로 파일로 만든거 load
+    :return:
+    """
+    with open('/Users/len/kamill/stopwords.txt', 'r', encoding='utf-8') as fp:
+        return [a.strip() for a in fp.readlines()]
