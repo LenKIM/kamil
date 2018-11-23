3c7b62648059c37f553a4c1f05b188c2b4f137fd
LenKIM <joenggyu0@gmail.com>
2018-11-23 14:39:45 +0900

    complete project

--- kamil/main.py
+++ kamil/main.py
@@ -1,12 +1,62 @@
-# -*- coding: utf-8 -*-
-import os
 import re
 
-from sklearn.feature_extraction.text import CountVectorizer
-path_sentences = 'sentences.txt'
+from kamil.helpers.file_helpers import get_frequent_counted_companies_from_file, get_total_text_from_files
+from kamil.helpers.stop_word_helpers import get_stopwords_from_file
+from kamil.kamil_const import const_subsidiary_companies
 
-f_read = open(path_sentences, 'r', encoding='utf8')
-f_write = open('result_sentences.txt', 'w', encoding='utf8')
+stopwords = get_stopwords_from_file()
+removed_special_texts, origin_texts = get_total_text_from_files()
 
-related_company = ['효성', '더클래스효성', '에이에스씨', '효성티앤에스', '진흥기업', '효성아이티엑스','효성itx', '에프엠케이','FMK', '효성굿스프링스', '효성캐피탈', '효성인포메이션시스템', '효성트랜스월드', '갤럭시아디바이스', '신화인터텍', '신성자동차', '효성토요타', '신동진', '갤럭시아커뮤니케이션즈', '더프리미엄효성', '갤럭시아일렉트로닉스', '엔에이치테크', '갤럭시아에스엠', '엔에이치씨엠에스', '갤럭시아코퍼레이션', '효성에프엠에스', '효성프리미어모터스', '아이티엑스마케팅', '평창풍력발전', '세빛섬', '공덕개발', '광주일보사', '공덕경우개발', '트리니티에셋매니지먼트', '에이티엠플러스', '행복두드리미', '태안솔라팜', '에브리쇼', '동륭실업', '아승오토모티브그룹', '효성투자개발', '효성중공업']
+passed_sentences = open('valid_sentences.txt', 'w', encoding='utf8')
+not_passed_sentences = open('valid_not_sentences.txt', 'w', encoding='utf8')
+not_yet_passed_sentences = open('unsure_sentences.txt', 'w', encoding='utf8')
 
+must_words = []
+must_not_words = ["실효성", "유효성", "전효성", "이효성", "유효성분", "원효성지", "발효성분"]
+must_not_words = '|'.join(must_not_words)
+must_not_only_words_pattern = '(' + must_not_words + ')'
+
+stop_words_pattern = '|'.join(stopwords)
+must_not_words_pattern = '(' + must_not_words + ')(' + stop_words_pattern + ')'
+frequency_company = get_frequent_counted_companies_from_file()
+ordered_companies = sorted(const_subsidiary_companies, key=lambda a: len(a), reverse=True)
+
+for company in ordered_companies:
+    subsidiary_company_with_stop_words_pattern = '^' + company + f'({stop_words_pattern})?$'
+    subsidiary_company_with_stop_words_pattern = re.compile(subsidiary_company_with_stop_words_pattern)
+
+    for key, value in frequency_company:
+        if re.match(subsidiary_company_with_stop_words_pattern, key) is not None:
+            must_words.append(key)
+
+must_words_text = '|'.join(must_words)
+must_only_words_pattern = '(' + must_words_text + ')'
+must_words_text_pattern = '(' + must_words_text + ')+(' + stop_words_pattern + ')'
+
+# 전처리를 위한 패턴 생성 완료
+for index, text in enumerate(removed_special_texts):
+    if re.search(must_not_words_pattern, text):
+        result = str(origin_texts[index]).strip() + '0' + '\n'
+        not_passed_sentences.write(result)
+    elif re.search(must_not_only_words_pattern, text):
+        result = str(origin_texts[index]).strip() + '0' + '\n'
+        not_passed_sentences.write(result)
+    elif re.search(must_words_text_pattern, text):
+        result = str(origin_texts[index]).strip() + '1' + '\n'
+        passed_sentences.write(result)
+    elif re.search(must_only_words_pattern, text):
+        result = str(origin_texts[index]).strip() + '1' + '\n'
+        passed_sentences.write(result)
+    else:
+        not_yet_passed_sentences.write(origin_texts[index])
+
+passed_sentences.close()
+not_passed_sentences.close()
+not_yet_passed_sentences.close()
+# 라벨 0
+# 라벨 1
+
+# Not yet
+
+
+# 각 종목들.
